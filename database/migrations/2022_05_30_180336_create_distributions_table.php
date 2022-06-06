f20738bbbc384032331f115ba331ec43e94a4c0e
firdauliiaa15 <firdauliiaa2@gmail.com>
2022-06-06 20:54:00 +0800

    Menambahkan distributions tabel

--- database/migrations/2022_05_30_180336_create_distributions_table.php
+++ database/migrations/2022_05_30_180336_create_distributions_table.php
@@ -3,3 +3,33 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
+class CreateDistributionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('distributions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('no_penyaluran');
+            $table->string('nama_mustahik');
+            $table->double('jumlah_zakat');
+            $table->string('nama_amil');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('distributions');
+    }
+}
