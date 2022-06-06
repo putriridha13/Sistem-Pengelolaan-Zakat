94cd87e450120cf428488913e10d4b0a46578e8f
firdauliiaa15 <firdauliiaa2@gmail.com>
2022-06-06 21:40:22 +0800

    Menambahkan Database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,32 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-
+    public function run()
+    {
+        User::create([
+            'name'      => 'Admin',
+            'username'  => 'admin',
+            'roles'     => 'admin',
+            'password'  => Hash::make('admin'),
+            'email'     => 'admin@gmail.com',
+            'picture'   => 'user.png'
+        ]);
+        User::create([
+            'name'      => 'Amil',
+            'username'  => 'amil',
+            'roles'     => 'amil',
+            'password'  => Hash::make('amil'),
+            'email'     => 'amil@gmail.com',
+            'picture'   => 'user.png'
+        ]);
+        User::create([
+            'name'      => 'Muzakki',
+            'username'  => 'muzakki',
+            'roles'     => 'muzakki',
+            'password'  => Hash::make('muzakki'),
+            'email'     => 'muzakki@gmail.com',
+            'picture'   => 'user.png'
+        ]);
+        User::factory(50)->create();
+    }
 }
