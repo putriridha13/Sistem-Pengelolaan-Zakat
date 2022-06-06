94cda139624b431121463e57cf75e73feeb75497
firdauliiaa15 <firdauliiaa2@gmail.com>
2022-06-06 20:48:53 +0800

    Menambahkan receptions tabel

--- database/migrations/2022_05_30_153658_create_receptions_table.php
+++ database/migrations/2022_05_30_153658_create_receptions_table.php
@@ -3,3 +3,33 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
+class CreateReceptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receptions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('no_pembayaran');
+            $table->string('nama_muzakki');
+            $table->double('jumlah_zakat');
+            $table->string('nama_amil');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receptions');
+    }
+}
