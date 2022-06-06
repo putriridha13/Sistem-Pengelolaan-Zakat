ef4bf0edfa6f4e13b70e3ed58885c04af1dd57bf
firdauliiaa15 <firdauliiaa2@gmail.com>
2022-06-06 21:24:57 +0800

    Menambahkan table artikel

--- database/migrations/2022_06_01_173052_create_articles_table.php
+++ database/migrations/2022_06_01_173052_create_articles_table.php
@@ -3,3 +3,33 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->string('picture');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
